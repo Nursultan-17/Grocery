6ee143b56857e2137814921469c40c56344b7cbd
Nursultan-17 <Nuroneguy@gmail.com>
2023-11-09 15:41:06 +0600

    grocery nickname update

--- store/views.py
+++ store/views.py
@@ -4,47 +4,61 @@ from django.contrib.auth import logout
 from django.http import HttpResponse
 from .models import *
 
-
-def homeView(request):
-    if 'cart' in request.session.keys():
-        context = {
-            'categories': Category.objects.all(),
-            'cart': request.session['cart']
-        }
-    else:
-        context = {
-            'categories': Category.objects.all()
-        }
-    return render(request=request, template_name='home.html', context=context)
-
-
 def signInView(request):
     if request.method == 'GET':
         context = {
-            'categories': Category.objects.all()
+            'categories': Category.objects.all(),
+
+
         }
         return render(request=request, template_name='sign_in.html', context=context)
     elif request.method == 'POST':
+        Nickname = request.POST.get('Nickname')
         email = request.POST.get('email')
         password = request.POST.get('password')
         user = authenticate(email=email, password=password)
         # authenticate - Если найден юзер в БД с таким имейлом и паролем то возвращает его, иначе None
         if user is not None:
             login(request, user)
-            # login - Авторизует юзера user и дает ему csrf_token, sessionid
+            if 'Nickname' not in request.session.keys():
+                request.session['Nickname'] = Nickname
+                request.session.modified = True
             return redirect('home_url')
         context = {
             'error': 'Не верный логин и/или пароль',
             'email': email,
-            'categories': Category.objects.all()
+            'categories': Category.objects.all(),
+            'Nickname': request.session['Nickname'],
+
         }
         return render(request=request, template_name='sign_in.html', context=context)
+
+
+def homeView(request):
+    if 'cart' in request.session.keys():
+        context = {
+            'categories': Category.objects.all(),
+            'cart': request.session['cart'],
+            'Nickname': request.session['Nickname'],
+
+        }
+    elif 'Nickname' in request.session.keys():
+            context = {
+                'categories': Category.objects.all(),
+                'Nickname': request.session['Nickname'],
+            }
+    else:
+            context = {
+                'categories': Category.objects.all(),
+
+        }
+    return render(request=request, template_name='home.html', context=context)
 
 
 def signUpView(request):
     if request.method == 'GET':
         context = {
-            'categories': Category.objects.all()
+            'categories': Category.objects.all(),
         }
         return render(request=request, template_name='sign_up.html', context=context)
     elif request.method == 'POST':
@@ -75,13 +89,13 @@ def signUpView(request):
                 'last_name': last_name,
                 'phone': phone,
                 'birth_date': birth_date,
-                'categories': Category.objects.all()
+                'categories': Category.objects.all(),
             }
             return render(request=request, template_name='sign_up.html', context=context)
 
 
 def signOutView(request):
-    logout(request)  # Встроенная функция, которая выкидывает из системы юзера(Видит юзера по request.user)
+    logout(request)# Встроенная функция, которая выкидывает из системы юзера(Видит юзера по request.user)
     return redirect('home_url')  # Перенаправляем по url 'home_url'
 
 
@@ -89,7 +103,8 @@ def productsView(request):
     context = {
         'categories': Category.objects.all(),
         'products': Product.objects.all(),
-        'category': 'All Products'
+        'category': 'All Products',
+        'Nickname': request.session['Nickname'],
     }
     return render(request=request, template_name='products.html', context=context)
 
@@ -107,6 +122,7 @@ def cartDetailView(request):
     if request.method == 'GET':
         context = {
             'categories': Category.objects.all(),
+            'Nickname': request.session['Nickname'],
         }
         total = 0
         if 'cart' in request.session.keys():
@@ -130,7 +146,8 @@ def cartDetailView(request):
         else:
             context = {
                 'categories': Category.objects.all(),
-                'error': 'Balance on you Wallet is not enough!'
+                'error': 'Balance on you Wallet is not enough!',
+                'Nickname': request.session['Nickname'],
             }
             if 'cart' in request.session.keys():
                 context['cart'] = []
@@ -148,6 +165,7 @@ def profileView(request):
     if request.user.is_authenticated:
         context = {
             'categories': Category.objects.all(),
+            'Nickname': request.session['Nickname'],
         }
         return render(request=request, template_name='profile.html', context=context)
     return redirect('sign_in_url')
@@ -159,9 +177,14 @@ def productsByCategoryView(request, category_id):
     context = {
         'categories': Category.objects.all(),
         'products': products,
-        'category': category.name
+        'category': category.name,
+        'Nickname': request.session['Nickname'],
     }
     return render(request=request, template_name='products.html', context=context)
 
 def about_us_view(request):
-    return render(request=request,template_name='about_us.html')
+    context = {
+        'categories': Category.objects.all(),
+        'Nickname': request.session['Nickname'],
+    }
+    return render(request=request,template_name='about_us.html',context=context)
